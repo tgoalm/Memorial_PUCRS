f3f42add017c7352606d1d718447180b1d5166c7
Tiago Almeida <tialmeid@gmail.com>
2024-02-04 20:06:33 -0300

    Finalização do tópico 7.

--- Projeto_em_Desenvolvimento_de_Software_WEB/7.01.projeto-aula/index.js
+++ Projeto_em_Desenvolvimento_de_Software_WEB/7.01.projeto-aula/index.js
@@ -1,8 +1,30 @@
 require('dotenv').config()
+// const mysql = require('mysql');
 const cool = require('cool-ascii-faces')
 const express = require('express')
 const port = process.env.PORT || 3001
 const app = express()
+
+// const mysqlConfig = {
+//     user: process.env.DB_USER,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASS,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     authentication: {
+//         type: 'default'
+//     }
+// };
+
+// app.getMaxListeners('/data', (req, res) => {
+//     connection.query(
+//         "select * from [dbo].[user]",
+//         (error, results, fields) => {
+//             if(error) throw error;
+//             res.json(results);
+//         }
+//     )
+// });
 
 app.get('/', (req, res) => {
     console.log('New feature.');
