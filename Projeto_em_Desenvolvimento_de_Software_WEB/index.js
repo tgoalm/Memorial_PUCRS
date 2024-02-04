c77c06f7caec6c7471df04465906f166e4f7939c
Tiago Almeida <tialmeid@gmail.com>
2024-02-04 09:57:50 -0300

    new feature 3

--- Projeto_em_Desenvolvimento_de_Software_WEB/index.js
+++ Projeto_em_Desenvolvimento_de_Software_WEB/index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     console.log('New feature.');
 });
 
+app.get('/feature2', (req, res) => {
+    console.log('New feature3');
+});
+
 app.listen(port, () => {
     console.log(`Start listening ${port}.`);
 });
